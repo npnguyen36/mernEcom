b9081d488a31228ab9620ff06afb1cc7ddf2daf9
npnguyen36 <npnguyen36@yahoo.com>
2024-09-10 22:38:34 -0400

    connect database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 require('dotenv').config()
-
+const dbConnect = require('./config/dbconnect')
 
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+dbConnect()
 
 app.use('/', (req, res) => { res.send('SERVER ONN') })
 
