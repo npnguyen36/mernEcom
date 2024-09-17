29bee325d52570bc5f5b391000ce07492ff6273b
npnguyen36 <npnguyen36@yahoo.com>
2024-09-16 23:03:42 -0400

    add errorHandler and hash password

---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+const { notFound, errHandler } = require('../middlewares/errHandler')
+const userRouter = require('./user')
+
+
+const initRoutes = (app) => { 
+    app.use('/api/user', userRouter)
+
+
+    app.use(notFound)
+    app.use(errHandler)
+ }
+
+ module.exports = initRoutes
